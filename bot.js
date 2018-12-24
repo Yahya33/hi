4dc7fb3d213ff17c35b9b73f80a46195742f4ee4
Yahya33 <44969997+Yahya33@users.noreply.github.com>
2018-12-24 17:22:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
+var server = "526772585015541765"; // ايدي السررفر
 var channel = "509357857666236434";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
